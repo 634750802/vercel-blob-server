73802af5f9c73bb114ea78087081860b93b09332
Mateusz Sarnowski <sarnowskimateusz86@gmail.com>
2024-12-19 13:38:49 +0100

    feature/add check if metaFile exist, refactor delete handler

--- src/handlers/del.ts
+++ src/handlers/del.ts
@@ -10,25 +10,24 @@ export default defineHandler({
   },
 
   async handle(requestUrl, request) {
-    const body = await request.json();
+    const body: { urls: string[] } = await request.json();
 
     const urlsArray = body.urls;
 
     if (urlsArray.length) {
       for (let url of urlsArray) {
-        const formattedUrl = url.replace(requestUrl.origin + '/', '');
+        const pathnameFromOrigin = url.replace(requestUrl.origin, '');
+        const fileUrl = path.join(storePath, pathnameFromOrigin);
+        const metaFileUrl = fileUrl + '._vercel_mock_meta_';
 
-        const file = Bun.file(path.join(storePath, formattedUrl));
-        const isExists = await file.exists();
+        const file = Bun.file(fileUrl);
+        const metaFile = Bun.file(metaFileUrl);
 
-        if (isExists) {
-          await unlink(path.join(storePath, formattedUrl));
-          await unlink(
-            path.join(storePath, formattedUrl + '._vercel_mock_meta_')
-          );
-          console.log(path.join(storePath, formattedUrl));
-        } else {
-          console.log('file not exists', formattedUrl);
+        if (await file.exists()) {
+          await unlink(fileUrl);
+        }
+        if (await metaFile.exists()) {
+          await unlink(metaFileUrl);
         }
       }
     }
